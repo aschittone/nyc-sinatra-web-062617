0eab48e89de8a360a6342be377f35915ac575182
github username <github email address>
2017-07-20 10:37:34 -0400

    switching drivers

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,15 @@
+class FiguresController < ApplicationController
+
+# /figures, to show all figures (no form)
+# /figures/new to create new figure, with title, landmark (neeed post request)
+# need check boxes for landmarks, titles on figures new page
+# /figures/:id, see a single figure (neeed post request)
+# /figures/:id/edit (neeed post request)
+
+	get '/figures' do
+		@figures = Figure.all
+		erb :index
+	end
+	
+
+end
