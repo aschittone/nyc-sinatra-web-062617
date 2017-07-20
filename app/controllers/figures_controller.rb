95bac2d7fd0c2dff25d048d18886e1aeaa8c23d5
github username <github email address>
2017-07-20 14:26:22 -0400

    switch

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class FiguresController < ApplicationController
 
 # /figures, to show all figures (no form)
@@ -16,6 +18,31 @@ class FiguresController < ApplicationController
 		@titles = Title.all
 		erb :'figures/new'
 	end
+
+	get '/figures/:id' do
+		# binding.pry
+		@figure = Figure.find(params[:id])
+		erb :'figures/show'
+	end
+
+	post '/figures' do
+		# {figure: {name: '...', land_mark_ids: [2,4,6], title_ids: [1,2]}}
+		@figure = Figure.create(name: params[:figure][:name])
+		if !params["landmark"]["name"].empty?
+			@figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+		end
+		# will add in year_built form, and logic
+		if !params["title"]["name"].empty?
+			@figure.titles << Title.create(name: params["title"]["name"])
+		end
+		# binding.pry
+		@figure.save
+		@figure.title_ids=(params[:figure]["title_ids"])
+		@figure.landmark_ids=(params[:figure]["landmark_ids"])
+		binding.pry
+		redirect "/figures/#{@figure.id}"
+	end
+
 
 
 end
